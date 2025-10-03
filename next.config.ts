fa269b78fd6b144fb2754c79c00ee9013dd93ffc
Merel Jacobs <merel.burleigh@gmail.com>
2025-10-03 11:00:16 -0700

    unprotexted rotue

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
